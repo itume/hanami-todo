33cc38191b8e0758ca292f3c2b9f6ac3b6a82753
itume <jem90ham@gmail.com>
2018-03-24 14:29:08 +0900

    use test record in list_todo test

--- spec/web/features/list_todos_spec.rb
+++ spec/web/features/list_todos_spec.rb
@@ -1,7 +1,15 @@
 require 'features_helper'
 
 describe 'List todos' do
-  it 'displays each todos on the page' do
+    let(:repository) { TodoRepository.new }
+    before do
+        repository.clear
+    
+        repository.create(title: 'テスト', description: 'テストで作成')
+        repository.create(title: 'テスト2',   descrption: 'テストテスト')
+    end
+
+    it 'displays each todos on the page' do
     visit '/todos'
 
     within '#todos' do
