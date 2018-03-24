d0700035149648f4b8d5b4a9e3752eafa8ebad69
itume <jem90ham@gmail.com>
2018-03-24 14:34:27 +0900

    use sample recoord in todos#index_spec

--- spec/web/views/todos/index_spec.rb
+++ spec/web/views/todos/index_spec.rb
@@ -6,10 +6,29 @@ describe Web::Views::Todos::Index do
   let(:view)      { Web::Views::Todos::Index.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    skip 'This is an auto-generated test. Edit it and add your own tests.'
+  it 'exposes #todo' do
+    view.todo.must_equal exposures.fetch(:todos)
+  end
 
-    # Example
-    view.foo.must_equal exposures.fetch(:foo)
+  describe 'when there are no todos' do
+    it 'shows a placeholder message' do
+      rendered.must_include('<p class="placeholder">There are no todos yet.</p>')
+    end
+  end
+
+  describe 'when there are todos' do
+    let(:todo1)     { Todo.new(title: 'Todo1', author: 'todo') }
+    let(:todo2)     { Todo.new(title: 'Todo2', author: 'todotodo') }
+    let(:exposures) { Hash[todos: [todo1, todo2]] }
+
+    it 'lists them all' do
+      rendered.scan(/class="todo"/).count.must_equal 2
+      rendered.must_include('Todo1')
+      rendered.must_include('Todo2')
+    end
+
+    it 'hides the placeholder message' do
+      rendered.wont_include('<p class="placeholder">There are no todos yet.</p>')
+    end
   end
 end
